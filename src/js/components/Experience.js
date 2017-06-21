b38a0ed7de1d52e26c90daafc33dab0dc663cc52
Justin Ho <mujan5427@gmail.com>
2017-06-21 17:57:04 +0800

    [update] CSS of Index and Experience components for RWD

--- src/js/components/Experience.js
+++ src/js/components/Experience.js
@@ -9,25 +9,25 @@ class Experience extends React.Component {
     return (
       <div className='experience-list'>
         <div>
-          <a>
+          <a href>
             <img className='experience-image' src='/assets/product1.jpg' />
             <figcaption className='experience-title'>遙望福爾摩沙的純粹，獨木舟敞洋忘憂藍海</figcaption>
+            <div className='experience-description'>
+              <span>TWD 1,599</span>
+              <a className='button-heart' href><i className='fa fa-heart-o' aria-hidden='true'></i></a>
+            </div>
           </a>
-          <div className='experience-description'>
-            <span>TWD 1,599</span>
-            <a href><i className='fa fa-heart-o' aria-hidden='true'></i></a>
-          </div>
         </div>
 
         <div>
-          <a>
+          <a href>
             <img className='experience-image' src='/assets/product3.jpg' />
             <figcaption className='experience-title'>來漢諾威馬場當個一日牛仔吧！</figcaption>
+            <div className='experience-description'>
+              <span>TWD 2,500</span>
+              <a className='button-heart' href><i className='fa fa-heart-o' aria-hidden='true'></i></a>
+            </div>
           </a>
-          <div className='experience-description'>
-            <span>TWD 2,500</span>
-            <a href><i className='fa fa-heart-o' aria-hidden='true'></i></a>
-          </div>
         </div>
       </div>
     );
