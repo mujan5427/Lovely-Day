99f7355265be95e87418e66d7f31b916bbec42e9
Justin Ho <mujan5427@gmail.com>
2017-06-25 17:54:40 +0800

    [add] Filter Panel and Picker of Filter elements

--- src/js/components/layout/Search.js
+++ src/js/components/layout/Search.js
@@ -5,15 +5,127 @@ import Filter from '../button/Filter';
 class Search extends React.Component {
   constructor(props) {
     super(props);
+
+    this.searchCSS = {
+      'filter-picker-region': {
+        'layout': {
+          'margin-left': '30px'
+        }
+      },
+      'filter-picker-type': {
+        'layout': {
+          'margin-left': '120px'
+        }
+      }
+    }
   }
 
   render() {
     return (
-      <div className='content'>
-        <section>
-          <Experience />
+      <div>
+        <div className='content'>
+          <section className='search-experience-list-panel'>
+            <Experience />
+          </section>
+          <Filter />
+        </div>
+
+        {/* Filter Panel */}
+        <section className='search-filter-panel'>
+          <div>
+            <label>地區</label>
+            <i className='fa fa-angle-down' aria-hidden='true'></i>
+          </div>
+          <div>
+            <label>類型</label>
+            <i className='fa fa-angle-down' aria-hidden='true'></i>
+          </div>
         </section>
-        <Filter />
+
+        {/* Picker of Filter for region */}
+        <div className='search-filter-picker-background'>
+          <div className='search-filter-picker-box' style={ this.searchCSS['filter-picker-region'].layout }>
+            <section>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='region-1' type='checkbox' />
+                <label htmlFor='region-1'>大台北</label>
+              </div>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='region-2' type='checkbox' />
+                <label htmlFor='region-2'>桃竹苗</label>
+              </div>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='region-3' type='checkbox' />
+                <label htmlFor='region-3'>宜蘭</label>
+              </div>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='region-4' type='checkbox' />
+                <label htmlFor='region-4'>中彰投</label>
+              </div>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='region-5' type='checkbox' />
+                <label htmlFor='region-5'>雲嘉南</label>
+              </div>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='region-6' type='checkbox' />
+                <label htmlFor='region-6'>高屏</label>
+              </div>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='region-7' type='checkbox' />
+                <label htmlFor='region-7'>花東</label>
+              </div>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='region-8' type='checkbox' />
+                <label htmlFor='region-8'>離島</label>
+              </div>
+            </section>
+
+            <section>
+              <a href>取消</a>
+              <a className='href-highlight' href>確定</a>
+            </section>
+          </div>
+        </div>
+
+        {/* Filter Picker for type */}
+        <div className='search-filter-picker-background'>
+          <div className='search-filter-picker-box' style={ this.searchCSS['filter-picker-type'].layout }>
+            <section>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='type-1' type='checkbox' />
+                <label htmlFor='type-1'>南澳生活節</label>
+              </div>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='type-2' type='checkbox' />
+                <label htmlFor='type-2'>夏令營專區</label>
+              </div>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='type-3' type='checkbox' />
+                <label htmlFor='type-3'>藝文手作</label>
+              </div>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='type-4' type='checkbox' />
+                <label htmlFor='type-4'>玩樂廚房</label>
+              </div>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='type-5' type='checkbox' />
+                <label htmlFor='type-5'>愛上戶外</label>
+              </div>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='type-6' type='checkbox' />
+                <label htmlFor='type-6'>親子專區</label>
+              </div>
+              <div className='checkbox form-component-theme-gray'>
+                <input id='type-7' type='checkbox' />
+                <label htmlFor='type-7'>情人專區</label>
+              </div>
+            </section>
+            <section>
+              <a href>取消</a>
+              <a className='href-highlight' href>確定</a>
+            </section>
+          </div>
+        </div>
       </div>
     );
   }
