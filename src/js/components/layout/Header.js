4173cf758f9e3c22b2320c25c909e1e3c1e9b6fd
Justin Ho <mujan5427@gmail.com>
2017-06-27 18:08:53 +0800

    [add] new elements into Header component for Menu Navigation of desktop version

--- src/js/components/layout/Header.js
+++ src/js/components/layout/Header.js
@@ -31,10 +31,50 @@ class Header extends React.Component {
 
           <nav>
             <a href><i className='fa fa-search' aria-hidden='true'></i></a>
-            <a className='hasIcon' href>
+            <a className='header-menu-navigation hasIcon' href>
               類型
               <i className='fa fa-angle-down' aria-hidden='true'></i>
             </a>
+
+            {/* Drop-Down-Menu Main */}
+            <div className='drop-down-menu-wrapper drop-down-menu-type-2'>
+              <div>
+                <section>
+                  <h1>最新的 南澳生活節</h1>
+                  <div>
+                    <a href>
+                      <img src='/assets/product2.jpg' />
+                      <figcaption>遙望福爾摩沙的純粹，獨木舟敞洋忘憂藍海</figcaption>
+                    </a>
+                    <a href>
+                      <img src='/assets/product4.jpg' />
+                      <figcaption>遙望基隆嶼，敞洋最美麗的東北角海域！</figcaption>
+                    </a>
+                    <a href>
+                      <img src='/assets/product5.jpg' />
+                      <figcaption>墾丁國家公園，帆船之旅，日初東方至晚霞染起</figcaption>
+                    </a>
+                    <a href>
+                      <img src='/assets/product6.jpg' />
+                      <figcaption>鹿野高台，熱氣球遨遊天際</figcaption>
+                    </a>
+                  </div>
+                </section>
+                <section>
+                  <nav>
+                    <a href>南澳生活節</a>
+                    <a href>夏令營專區</a>
+                    <a href>藝文手作</a>
+                    <a href>玩樂廚房</a>
+                    <a href>愛上戶外</a>
+                    <a href>親子專區</a>
+                    <a href>團體遊戲</a>
+                    <a href>情人專區</a>
+                  </nav>
+                </section>
+              </div>
+            </div>
+
             <a href>幫助</a>
 
             {/* has logged out */}
