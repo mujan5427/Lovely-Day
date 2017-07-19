4d196a7535c3ac9c34740f349b08c627cce57408
Justin Ho <mujan5427@gmail.com>
2017-07-19 23:11:14 +0800

    [add] logic of get experience list API

--- server-side/controller/experience.js
+++ server-side/controller/experience.js
@@ -1,6 +1,17 @@
-const experience = require('../module/experience');
+const experience   = require('../module/experience');
+const verification = require('../helpers/verification');
+
 
 exports.getExperienceList = function(req, res) {
+  var inputData           = Object.assign({}, req.query, req.headers)
+  const headerColumnName  = ['member_id', 'token'];
+  const requestColumnName = ['item_limit', 'current_page'];
+  const columnName        = headerColumnName.concat(requestColumnName);
 
-  experience.getAllExperience(res);
+  if (!verification.verifyInputColumn(columnName, inputData)) {
+
+    res.status(403).end();
+  } else {
+    experience.getAllExperience(inputData, res);
+  }
 };
--- server-side/module/experience.js
+++ server-side/module/experience.js
@@ -1,17 +1,35 @@
 const db = require('./database');
 
-// This is example of experience module
+
 var experience = {
-  getAllExperience: function (res) {
-    db.query(`SELECT name, price FROM experience`, (error, rows) => {
+  getAllExperience: function (inputData, res) {
+    const sqlStatement = `
+      SELECT experience.*,  IF(favorite.favorited, 'true', 'false') as favorited
+      FROM experience
+      LEFT JOIN favorite ON experience.id = favorite.experience_id AND favorite.account_id = ?
+      LEFT JOIN account ON account.id = favorite.account_id
+      ORDER BY id ASC
+      LIMIT ?, ?`;
 
-      var responseData = {
-        dataCount: rows.length,
-        item: rows
-      };
+    var accountId   = Number(inputData.member_id);
+    var itemLimit   = Number(inputData.item_limit);
+    var currentPage = Number(inputData.current_page) <= 1 ? 0 : (Number(inputData.current_page) - 1) * itemLimit;
 
-      res.json(responseData);
-      db.end();
+    const sqlPlaceholder = [accountId, currentPage, itemLimit];
+
+    db.query(sqlStatement, sqlPlaceholder, (error, rows) => {
+
+      if (rows.length > 0) {
+        var responseData = {
+          dataCount: rows.length,
+          item: rows.map(row => Object.assign({}, row, {favorited: row.favorited === 'true'}))
+        };
+
+        res.json(responseData);
+      } else {
+        res.status(403).end();
+
+      }
     });
   }
 };
