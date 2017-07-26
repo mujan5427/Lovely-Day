329da02dd69b273fbbac30b0d61e9e9165630cf5
Justin Ho <mujan5427@gmail.com>
2017-07-26 15:07:56 +0800

    [add] error config file

---
+++ server-side/error_config.js
@@ -0,0 +1,18 @@
+const error_config =
+{
+  client: {
+    1: {
+      message: 'You miss some properties'
+    },
+    2: {
+      message: 'Some properties are empty'
+    }
+  },
+  database: {
+    1: {
+      message: 'Didn\'t search for anything'
+    }
+  }
+};
+
+module.exports = error_config;
