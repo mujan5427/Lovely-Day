abc9932d4ca5c139d36c97b3482863d3ddf17994
Justin Ho <mujan5427@gmail.com>
2017-07-27 13:14:14 +0800

    [update] apply the error capture mechanism

--- server-side/controller/favourite.js
+++ server-side/controller/favourite.js
@@ -1,7 +1,8 @@
+const error        = require('../helpers/error');
 const favourite    = require('../module/favourite');
 const verification = require('../helpers/verification');
 
-
+// Required : member_id、token、experience_id
 exports.addFavourite = function(req, res) {
 
   var inputData           = Object.assign({}, req.body, req.headers)
@@ -9,20 +10,17 @@ exports.addFavourite = function(req, res) {
   const requestColumnName = ['experience_id'];
   const columnName        = headerColumnName.concat(requestColumnName);
 
-  if (!verification.verifyColumnIsExist(columnName, inputData)) {
+  try {
+    verification.verifyColumnIsExist(columnName, inputData);
+    verification.verifyToken(inputData.member_id, inputData.token);
+    favourite.add(inputData, res);
 
-    res.status(403).end();
-  } else {
-    if (!verification.verifyToken(inputData.member_id, inputData.token)) {
-
-      res.status(403).end();
-    } else {
-
-      favourite.add(inputData, res);
-    }
+  } catch(err) {
+    error.analysisErrorObject(err, res);
   }
 };
 
+// Required : member_id、token、experience_id
 exports.deleteFavourite = function(req, res) {
 
   var inputData           = Object.assign({}, req.body, req.headers)
@@ -30,16 +28,12 @@ exports.deleteFavourite = function(req, res) {
   const requestColumnName = ['experience_id'];
   const columnName        = headerColumnName.concat(requestColumnName);
 
-  if (!verification.verifyColumnIsExist(columnName, inputData)) {
+  try {
+    verification.verifyColumnIsExist(columnName, inputData);
+    verification.verifyToken(inputData.member_id, inputData.token);
+    favourite.delete(inputData, res);
 
-    res.status(403).end();
-  } else {
-    if (!verification.verifyToken(inputData.member_id, inputData.token)) {
-
-      res.status(403).end();
-    } else {
-
-      favourite.delete(inputData, res);
-    }
+  } catch(err) {
+    error.analysisErrorObject(err, res);
   }
 };
