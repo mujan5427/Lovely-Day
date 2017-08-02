5aaf0b65ab7bd59644e4d0d9e2705b1a0dbffbc0
Justin Ho <mujan5427@gmail.com>
2017-08-02 15:21:30 +0800

    [add] getCookie、deleteCookie of helpers function

--- src/js/helpers/cookie.js
+++ src/js/helpers/cookie.js
@@ -11,3 +11,32 @@ export function setCookie(memberInfo) {
     document.cookie = `${property}=${memberInfo[property]};expires=${expiratioDate}`;
   }
 };
+
+export function getCookie() {
+  var cookieString = document.cookie;
+  var cookieObject = {};
+
+  if (!isEmpty(cookieString)) {
+    var cookieArray  = cookieString.split(';');
+
+    cookieArray.map(cookie => {
+      var cookieKeyValuePair = cookie.split('=');
+      var cookieName = cookieKeyValuePair[0].trim();
+      var cookieValue = cookieKeyValuePair[1].trim();
+
+      Object.assign(cookieObject, {[cookieName]: cookieValue})
+    });
+  }
+
+  return cookieObject;
+};
+
+export function deleteCookie() {
+  var property;
+  const cookieObject  = getCookie();
+  const expiratioDate = 'Thu, 01 Jan 1970 00:00:00 UTC';
+
+  for(property in cookieObject) {
+    document.cookie = `${property}=;expires=${expiratioDate};path=/`;
+  }
+};
