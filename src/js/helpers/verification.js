7100ad5ecdc5b7c53366b2fd70b34d5bd32154b9
Justin Ho <mujan5427@gmail.com>
2017-08-08 21:11:58 +0800

    [update] validator function can directly return error message

--- src/js/helpers/verification.js
+++ src/js/helpers/verification.js
@@ -1,18 +1,28 @@
+import errorMessageConfig from '../errorMessageConfig';
+
 export const EMAIL    = 'email';
 export const PASSWORD = 'password';
 
 
 export function validator(type, source) {
-  var patten;
+  var patten, errorMessage;
 
-  switch(type) {
-    case EMAIL:
-      patten = /^\w+((-\w+)|(\.\w+))*\@\w+((\.|-)\w+)*\.+\w+$/g;
-      break;
-    case PASSWORD:
-      patten = /^[a-z0-9][a-z0-9]{3,7}$/g;
-      break;
+  if (isEmpty(source)) {
+    return errorMessageConfig[3];
+
+  } else {
+    switch(type) {
+      case EMAIL:
+        patten       = /^\w+((-\w+)|(\.\w+))*\@\w+((\.|-)\w+)*\.+\w+$/g;
+        errorMessage = errorMessageConfig[1];
+        break;
+
+      case PASSWORD:
+        patten       = /^[a-z0-9][a-z0-9]{3,7}$/g;
+        errorMessage = errorMessageConfig[2];
+        break;
+    }
+
+    return patten.test(source) ? true : errorMessage;
   }
-
-  return patten.test(source);
 };
