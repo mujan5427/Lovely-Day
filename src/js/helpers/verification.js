338cf270025cda177aee14fba8ab1a62549fb583
Justin Ho <mujan5427@gmail.com>
2017-08-10 17:35:34 +0800

    [update] executing sequence of logical operators

--- src/js/helpers/verification.js
+++ src/js/helpers/verification.js
@@ -32,7 +32,7 @@ export function verifyRequiredField(localState) {
   // Verify required field
   for(property in formDataLocalState) {
     if (formDataLocalState[property].hasOwnProperty('isRequired') &&
-        isEmpty(formDataLocalState[property].value) || formDataLocalState[property].value === false) {
+        (isEmpty(formDataLocalState[property].value) || formDataLocalState[property].value === false)) {
 
       needToModifiedState = {
         errorMessage: errorMessageConfig[3]
