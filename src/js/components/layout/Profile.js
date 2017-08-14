bbfe87135b5d06567cffc475d0f0a7046dbd01d7
Justin Ho <mujan5427@gmail.com>
2017-08-14 20:44:26 +0800

    [update] Profile component 1. integrate RadioBoxGroup、CheckBox component with its local state 2. add validation mechanism of form for required field

--- src/js/components/layout/Profile.js
+++ src/js/components/layout/Profile.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { changeFormState, hasErrorMessage } from '../../helpers/localState';
+import { verifyRequiredField, verifyNeedToVerifiedField } from '../../helpers/verification';
 import SelectBox from '../form/SelectBox';
 import InputBox from '../form/InputBox';
+import RadioBoxGroup from '../form/RadioBoxGroup';
+import CheckBox from '../form/CheckBox';
 
 class Profile extends React.Component {
   constructor(props) {
@@ -13,6 +16,7 @@ class Profile extends React.Component {
     const day   = birthday.split('-')[2];
 
     this.formElementEventHandler = this.formElementEventHandler.bind(this);
+    this.saveButton              = this.saveButton.bind(this);
 
 
     /* * * * * * * * * * * * *
@@ -28,10 +32,12 @@ class Profile extends React.Component {
        formData: {
          firstname: {
            value: firstname,
+           isRequired: true,
            errorMessage: ''
          },
          lastname: {
            value: lastname,
+           isRequired: true,
            errorMessage: ''
          },
          gender: {
@@ -61,12 +67,70 @@ class Profile extends React.Component {
            value: !isEmpty(educationLevel) ? educationLevel : '',
            errorMessage: ''
          },
-         interest: {
-           value: !isEmpty(interest) ? interest : '',
+         sport: {
+           value: this._parseInterest(interest, 'sport'),
+           errorMessage: ''
+         },
+         handmade: {
+           value: this._parseInterest(interest, 'hand_made'),
+           errorMessage: ''
+         },
+         baking: {
+           value: this._parseInterest(interest, 'baking'),
+           errorMessage: ''
+         },
+         art: {
+           value: this._parseInterest(interest, 'art'),
+           errorMessage: ''
+         },
+         history: {
+           value: this._parseInterest(interest, 'history'),
            errorMessage: ''
          }
        }
      };
+  }
+
+  _parseInterest(interest, specifiedProperty) {
+    const indexOfSearchResult = interest.indexOf(specifiedProperty);
+
+    if(indexOfSearchResult !== -1) {
+      return true;
+    }
+
+    return false;
+  }
+
+  saveButton() {
+    const { dispatch }                  = this.props;
+    const { firstname, lastname, month, day, year } = this.state.formData;
+    const validationRequiredField       = verifyRequiredField(this.state);
+    const validationNeedToVerifiedField = verifyNeedToVerifiedField(this.state);
+    var requestData                     = {};
+
+    // Verify required field
+    if (validationRequiredField.hasErrorMessage) {
+      this.setState(validationRequiredField.state);
+
+    // Verify need to verified field
+    } else if (validationNeedToVerifiedField.hasErrorMessage) {
+      this.setState(validationNeedToVerifiedField.state);
+
+    } else {
+      console.log(`全部驗證完成`);
+      // Complete all of validation step
+      // and
+      // dispatch specified API
+      // requestData = {
+      //   email     : email.value,
+      //   firstname : firstname.value,
+      //   lastname  : lastname.value,
+      //   password  : password.value,
+      //   birthday  : `${year.value}-${month.value}-${day.value}`
+      // };
+      //
+      // dispatch(signup(requestData));
+    }
   }
 
   formElementEventHandler(event) {
@@ -150,6 +214,52 @@ class Profile extends React.Component {
           state  = changeFormState(this.state, 'educationLevel', needToModifiedState);
           break;
 
+        case 'sport':
+          needToModifiedState = {
+            value: !this.state.formData.sport.value,
+            errorMessage: ''
+          };
+
+          state  = changeFormState(this.state, 'sport', needToModifiedState);
+          break;
+
+        case 'handmade':
+          needToModifiedState = {
+            value: !this.state.formData.handmade.value,
+            errorMessage: ''
+          };
+
+          state  = changeFormState(this.state, 'handmade', needToModifiedState);
+
+          break;
+
+        case 'baking':
+          needToModifiedState = {
+            value: !this.state.formData.baking.value,
+            errorMessage: ''
+          };
+
+          state  = changeFormState(this.state, 'baking', needToModifiedState);
+          break;
+
+        case 'art':
+          needToModifiedState = {
+            value: !this.state.formData.art.value,
+            errorMessage: ''
+          };
+
+          state  = changeFormState(this.state, 'art', needToModifiedState);
+          break;
+
+        case 'history':
+          needToModifiedState = {
+            value: !this.state.formData.history.value,
+            errorMessage: ''
+          };
+
+          state  = changeFormState(this.state, 'history', needToModifiedState);
+          break;
+
         default:
           break;
       }
@@ -160,7 +270,7 @@ class Profile extends React.Component {
 
   render() {
     const { email } = this.props;
-    const { firstname, lastname, gender, year, month, day, language, educationLevel, interest } = this.state.formData;
+    const { firstname, lastname, gender, year, month, day, language, educationLevel, sport, handmade, baking, art, history } = this.state.formData;
 
     return (
       <div className='content'>
@@ -174,11 +284,21 @@ class Profile extends React.Component {
           <div>
             <label>名字</label>
             <InputBox type='firstname' value={ firstname.value } errorMessage={ firstname.errorMessage } />
+
+            { !isEmpty(firstname.errorMessage) &&
+              <div className='form-error-message'>{ firstname.errorMessage }</div>
+            }
+
           </div>
 
           <div>
             <label>姓氏</label>
             <InputBox type='lastname' value={ lastname.value } errorMessage={ lastname.errorMessage } />
+
+            { !isEmpty(lastname.errorMessage) &&
+              <div className='form-error-message'>{ lastname.errorMessage }</div>
+            }
+
             <div className='profile-form-component-description'>您的公開個人資料僅會顯示您的名字。</div>
           </div>
 
@@ -195,6 +315,13 @@ class Profile extends React.Component {
               <SelectBox type='day' value={ day.value } errorMessage={ day.errorMessage } />
               <SelectBox type='year' value={ year.value } errorMessage={ year.errorMessage } />
             </div>
+
+            { (!isEmpty(month.errorMessage) || !isEmpty(day.errorMessage) || !isEmpty(year.errorMessage)) &&
+              <div className='form-error-message'>
+                { month.errorMessage || day.errorMessage || year.errorMessage }
+              </div>
+            }
+
             <div className='profile-form-component-description'>您來到這個世界的神奇日子。此數據僅作分析用途，不會透露給其他用戶。</div>
           </div>
 
@@ -217,26 +344,11 @@ class Profile extends React.Component {
           <div>
             <label>教育程度</label>
             <div className='profile-form-component-group'>
-              <div className='radiobox form-component-theme-gray'>
-                <input id='junior' type='radio' name='education-level' />
-                <label htmlFor='junior' data-element-name='educationLevel'>國中</label>
-              </div>
-              <div className='radiobox form-component-theme-gray'>
-                <input id='senior' type='radio' name='education-level' />
-                <label htmlFor='senior' data-element-name='educationLevel'>高中</label>
-              </div>
-              <div className='radiobox form-component-theme-gray'>
-                <input id='bachelor' type='radio' name='education-level' />
-                <label htmlFor='bachelor' data-element-name='educationLevel'>大學</label>
-              </div>
-              <div className='radiobox form-component-theme-gray'>
-                <input id='master' type='radio' name='education-level' />
-                <label htmlFor='master' data-element-name='educationLevel'>碩士</label>
-              </div>
-              <div className='radiobox form-component-theme-gray'>
-                <input id='doctor' type='radio' name='education-level' />
-                <label htmlFor='doctor' data-element-name='educationLevel'>博士</label>
-              </div>
+              <RadioBoxGroup
+                type='educationLevel'
+                selected={ educationLevel.value }
+                errorMessage={ educationLevel.errorMessage }
+              />
             </div>
             <div className='profile-form-component-description'>此數據僅作分析用途，不會透露給其他用戶。</div>
           </div>
@@ -244,32 +356,31 @@ class Profile extends React.Component {
           <div>
             <label>興趣類型</label>
             <div className='profile-form-component-group'>
-              <div className='checkbox form-component-theme-gray'>
-                <input id='sport' type='checkbox' />
-                <label htmlFor='sport'>運動</label>
-              </div>
-              <div className='checkbox form-component-theme-gray'>
-                <input id='handmade' type='checkbox' />
-                <label htmlFor='handmade'>手作</label>
-              </div>
-              <div className='checkbox form-component-theme-gray'>
-                <input id='baking' type='checkbox' />
-                <label htmlFor='baking'>烘焙</label>
-              </div>
-              <div className='checkbox form-component-theme-gray'>
-                <input id='camping' type='checkbox' />
-                <label htmlFor='camping'>露營</label>
-              </div>
-              <div className='checkbox form-component-theme-gray'>
-                <input id='history' type='checkbox' />
-                <label htmlFor='history'>歷史</label>
-              </div>
+              <CheckBox id='sport' value={ sport.value } errorMessage={ sport.errorMessage }>
+                <label htmlFor='sport' data-element-name='sport'>運動</label>
+              </CheckBox>
+
+              <CheckBox id='handmade' value={ handmade.value } errorMessage={ handmade.errorMessage }>
+                <label htmlFor='handmade' data-element-name='handmade'>手作</label>
+              </CheckBox>
+
+              <CheckBox id='baking' value={ baking.value } errorMessage={ baking.errorMessage }>
+                <label htmlFor='baking' data-element-name='baking'>烘焙</label>
+              </CheckBox>
+
+              <CheckBox id='art' value={ art.value } errorMessage={ art.errorMessage }>
+                <label htmlFor='art' data-element-name='art'>藝術</label>
+              </CheckBox>
+
+              <CheckBox id='history' value={ history.value } errorMessage={ history.errorMessage }>
+                <label htmlFor='history' data-element-name='history'>歷史</label>
+              </CheckBox>
             </div>
             <div className='profile-form-component-description'>此數據僅作分析用途，不會透露給其他用戶。</div>
           </div>
 
           <div>
-            <a className='button solid solid-theme-pink' href>儲存</a>
+            <a className='button solid solid-theme-pink' onClick={ this.saveButton }>儲存</a>
           </div>
 
         </section>
