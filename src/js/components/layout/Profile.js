bd49b1861fdc1e208e5fd9dae493b17c6518897c
Justin Ho <mujan5427@gmail.com>
2017-08-14 13:47:14 +0800

    [update] integrate Profile component with its local state without interest field

--- src/js/components/layout/Profile.js
+++ src/js/components/layout/Profile.js
@@ -1,227 +1,216 @@
 import React from 'react';
+import { changeFormState, hasErrorMessage } from '../../helpers/localState';
+import SelectBox from '../form/SelectBox';
+import InputBox from '../form/InputBox';
 
 class Profile extends React.Component {
   constructor(props) {
     super(props);
+
+    const { firstname, lastname, gender, birthday, language, educationLevel, interest } = this.props;
+    const year  = birthday.split('-')[0];
+    const month = birthday.split('-')[1];
+    const day   = birthday.split('-')[2];
+
+    this.formElementEventHandler = this.formElementEventHandler.bind(this);
+
+
+    /* * * * * * * * * * * * *
+     *                       *
+     *      Local State      *
+     *                       *
+     * * * * * * * * * * * * */
+
+    //  先透過 container component 轉換 app state 成 props
+    //  再將 props 嵌入 local state of Profile
+
+     this.state = {
+       formData: {
+         firstname: {
+           value: firstname,
+           errorMessage: ''
+         },
+         lastname: {
+           value: lastname,
+           errorMessage: ''
+         },
+         gender: {
+           value: !isEmpty(gender) ? gender : '',
+           errorMessage: ''
+         },
+         month: {
+           value: month,
+           isRequired: true,
+           errorMessage: ''
+         },
+         day: {
+           value: day,
+           isRequired: true,
+           errorMessage: ''
+         },
+         year: {
+           value: year,
+           isRequired: true,
+           errorMessage: ''
+         },
+         language: {
+           value: !isEmpty(language) ? language : '',
+           errorMessage: ''
+         },
+         educationLevel: {
+           value: !isEmpty(educationLevel) ? educationLevel : '',
+           errorMessage: ''
+         },
+         interest: {
+           value: !isEmpty(interest) ? interest : '',
+           errorMessage: ''
+         }
+       }
+     };
+  }
+
+  formElementEventHandler(event) {
+    const target      = event.target;
+    const elementName =
+    isEmpty(target.getAttribute('data-element-name')) ? false : target.getAttribute('data-element-name');
+
+    var state, needToModifiedState;
+
+    if (elementName) {
+      switch(elementName) {
+        case 'firstname':
+          needToModifiedState = {
+            value: target.value,
+            errorMessage: ''
+          };
+
+          state  = changeFormState(this.state, 'firstname', needToModifiedState);
+          break;
+
+        case 'lastname':
+          needToModifiedState = {
+            value: target.value,
+            errorMessage: ''
+          };
+
+          state  = changeFormState(this.state, 'lastname', needToModifiedState);
+          break;
+
+        case 'gender':
+          needToModifiedState = {
+            value: target.value,
+            errorMessage: ''
+          };
+
+          state  = changeFormState(this.state, 'gender', needToModifiedState);
+          break;
+
+        case 'month':
+          needToModifiedState = {
+            value: target.value,
+            errorMessage: ''
+          };
+
+          state  = changeFormState(this.state, 'month', needToModifiedState);
+          break;
+
+        case 'day':
+          needToModifiedState = {
+            value: target.value,
+            errorMessage: ''
+          };
+
+          state  = changeFormState(this.state, 'day', needToModifiedState);
+          break;
+
+        case 'year':
+          needToModifiedState = {
+            value: target.value,
+            errorMessage: ''
+          };
+
+          state  = changeFormState(this.state, 'year', needToModifiedState);
+          break;
+
+        case 'language':
+          needToModifiedState = {
+            value: target.value,
+            errorMessage: ''
+          };
+
+          state  = changeFormState(this.state, 'language', needToModifiedState);
+          break;
+
+        case 'educationLevel':
+          needToModifiedState = {
+            value: target.getAttribute('for'),
+            errorMessage: ''
+          };
+
+          state  = changeFormState(this.state, 'educationLevel', needToModifiedState);
+          break;
+
+        default:
+          break;
+      }
+
+      this.setState(state);
+    }
   }
 
   render() {
+    const { email } = this.props;
+    const { firstname, lastname, gender, year, month, day, language, educationLevel, interest } = this.state.formData;
+
     return (
       <div className='content'>
-        <section className='profile-section'>
+        <section
+          className='profile-section'
+          onChange={ this.formElementEventHandler }
+          onClick={ this.formElementEventHandler }
+        >
           <h1>個人資料</h1>
 
           <div>
             <label>名字</label>
-            <div className='input-box form-component-theme-gray'>
-              <input type='text' />
-            </div>
+            <InputBox type='firstname' value={ firstname.value } errorMessage={ firstname.errorMessage } />
           </div>
 
           <div>
             <label>姓氏</label>
-            <div className='input-box form-component-theme-gray'>
-              <input type='text' />
-            </div>
+            <InputBox type='lastname' value={ lastname.value } errorMessage={ lastname.errorMessage } />
             <div className='profile-form-component-description'>您的公開個人資料僅會顯示您的名字。</div>
           </div>
 
           <div>
             <label>性別</label>
-            <div className='selectbox'>
-              <select className='form-component-theme-gray'>
-                <option value=''>男性</option>
-                <option value=''>女性</option>
-              </select>
-            </div>
+            <SelectBox type='gender' value={ gender.value } errorMessage={ gender.errorMessage } />
             <div className='profile-form-component-description'>此數據僅作分析用途，不會透露給其他用戶。</div>
           </div>
 
           <div>
             <label>出生日期</label>
             <div className='selectbox-for-birthday'>
-              <div className='selectbox'>
-                <select className='form-component-theme-gray' value='feb'>
-                <option value=''>一月</option>
-                <option value='feb'>二月</option>
-                <option value=''>三月</option>
-                <option value=''>四月</option>
-                <option value=''>五月</option>
-                <option value=''>六月</option>
-                <option value=''>七月</option>
-                <option value=''>八月</option>
-                <option value=''>九月</option>
-                <option value=''>十月</option>
-                <option value=''>十一月</option>
-                <option value=''>十二月</option>
-              </select>
-              </div>
-              <div className='selectbox'>
-                <select className='form-component-theme-gray' value='2'>
-                <option value=''>1</option>
-                <option value='2'>2</option>
-                <option value=''>3</option>
-                <option value=''>4</option>
-                <option value=''>5</option>
-                <option value=''>6</option>
-                <option value=''>7</option>
-                <option value=''>8</option>
-                <option value=''>9</option>
-                <option value=''>10</option>
-                <option value=''>11</option>
-                <option value=''>12</option>
-                <option value=''>13</option>
-                <option value=''>14</option>
-                <option value=''>15</option>
-                <option value=''>16</option>
-                <option value=''>17</option>
-                <option value=''>18</option>
-                <option value=''>19</option>
-                <option value=''>20</option>
-                <option value=''>21</option>
-                <option value=''>22</option>
-                <option value=''>23</option>
-                <option value=''>24</option>
-                <option value=''>25</option>
-                <option value=''>26</option>
-                <option value=''>27</option>
-                <option value=''>28</option>
-                <option value=''>29</option>
-                <option value=''>30</option>
-                <option value=''>31</option>
-              </select>
-              </div>
-              <div className='selectbox'>
-                <select className='form-component-theme-gray' value='1989'>
-                <option value='1999'>1999</option>
-                <option value='1998'>1998</option>
-                <option value='1997'>1997</option>
-                <option value='1996'>1996</option>
-                <option value='1995'>1995</option>
-                <option value='1994'>1994</option>
-                <option value='1993'>1993</option>
-                <option value='1992'>1992</option>
-                <option value='1991'>1991</option>
-                <option value='1990'>1990</option>
-                <option value='1989'>1989</option>
-                <option value='1988'>1988</option>
-                <option value='1987'>1987</option>
-                <option value='1986'>1986</option>
-                <option value='1985'>1985</option>
-                <option value='1984'>1984</option>
-                <option value='1983'>1983</option>
-                <option value='1982'>1982</option>
-                <option value='1981'>1981</option>
-                <option value='1980'>1980</option>
-                <option value='1979'>1979</option>
-                <option value='1978'>1978</option>
-                <option value='1977'>1977</option>
-                <option value='1976'>1976</option>
-                <option value='1975'>1975</option>
-                <option value='1974'>1974</option>
-                <option value='1973'>1973</option>
-                <option value='1972'>1972</option>
-                <option value='1971'>1971</option>
-                <option value='1970'>1970</option>
-                <option value='1969'>1969</option>
-                <option value='1968'>1968</option>
-                <option value='1967'>1967</option>
-                <option value='1966'>1966</option>
-                <option value='1965'>1965</option>
-                <option value='1964'>1964</option>
-                <option value='1963'>1963</option>
-                <option value='1962'>1962</option>
-                <option value='1961'>1961</option>
-                <option value='1960'>1960</option>
-                <option value='1959'>1959</option>
-                <option value='1958'>1958</option>
-                <option value='1957'>1957</option>
-                <option value='1956'>1956</option>
-                <option value='1955'>1955</option>
-                <option value='1954'>1954</option>
-                <option value='1953'>1953</option>
-                <option value='1952'>1952</option>
-                <option value='1951'>1951</option>
-                <option value='1950'>1950</option>
-                <option value='1949'>1949</option>
-                <option value='1948'>1948</option>
-                <option value='1947'>1947</option>
-                <option value='1946'>1946</option>
-                <option value='1945'>1945</option>
-                <option value='1944'>1944</option>
-                <option value='1943'>1943</option>
-                <option value='1942'>1942</option>
-                <option value='1941'>1941</option>
-                <option value='1940'>1940</option>
-                <option value='1939'>1939</option>
-                <option value='1938'>1938</option>
-                <option value='1937'>1937</option>
-                <option value='1936'>1936</option>
-                <option value='1935'>1935</option>
-                <option value='1934'>1934</option>
-                <option value='1933'>1933</option>
-                <option value='1932'>1932</option>
-                <option value='1931'>1931</option>
-                <option value='1930'>1930</option>
-                <option value='1929'>1929</option>
-                <option value='1928'>1928</option>
-                <option value='1927'>1927</option>
-                <option value='1926'>1926</option>
-                <option value='1925'>1925</option>
-                <option value='1924'>1924</option>
-                <option value='1923'>1923</option>
-                <option value='1922'>1922</option>
-                <option value='1921'>1921</option>
-                <option value='1920'>1920</option>
-                <option value='1919'>1919</option>
-                <option value='1918'>1918</option>
-                <option value='1917'>1917</option>
-              </select>
-              </div>
+              <SelectBox type='month' value={ month.value } errorMessage={ month.errorMessage } />
+              <SelectBox type='day' value={ day.value } errorMessage={ day.errorMessage } />
+              <SelectBox type='year' value={ year.value } errorMessage={ year.errorMessage } />
             </div>
             <div className='profile-form-component-description'>您來到這個世界的神奇日子。此數據僅作分析用途，不會透露給其他用戶。</div>
           </div>
 
           <div>
             <label>電子郵件</label>
-            <div className='input-box form-component-theme-gray'>
-              <input type='email' />
-            </div>
+            <InputBox
+              type='email'
+              value={ email }
+              disabled={ true }
+            />
             <div className='profile-form-component-description'>我們不會向其他 Levely Day 用戶透露您的個人電子郵件地址。</div>
           </div>
 
           <div>
             <label>語言</label>
-            <div className='selectbox'>
-              <select className='form-component-theme-gray' value='zh-TW'>
-                <option value='id'>Bahasa Indonesia</option>
-                <option value='ms'>Bahasa Melayu</option>
-                <option value='ca'>Català</option>
-                <option value='da'>Dansk</option>
-                <option value='de'>Deutsch</option>
-                <option value='en'>English</option>
-                <option value='es'>Español</option>
-                <option value='el'>Eλληνικά</option>
-                <option value='fr'>Français</option>
-                <option value='it'>Italiano</option>
-                <option value='hu'>Magyar</option>
-                <option value='nl'>Nederlands</option>
-                <option value='no'>Norsk</option>
-                <option value='pl'>Polski</option>
-                <option value='pt'>Português</option>
-                <option value='fi'>Suomi</option>
-                <option value='sv'>Svenska</option>
-                <option value='tr'>Türkçe</option>
-                <option value='is'>Íslenska</option>
-                <option value='cs'>Čeština</option>
-                <option value='ru'>Русский</option>
-                <option value='th'>ภาษาไทย</option>
-                <option value='zh'>中文 (简体)</option>
-                <option value='zh-TW'>中文 (繁體)</option>
-                <option value='ja'>日本語</option>
-                <option value='ko'>한국어</option>
-              </select>
-            </div>
+            <SelectBox type='language' value={ language.value } errorMessage={ language.errorMessage } />
             <div className='profile-form-component-description'>我們會使用此語言向您發送訊息。</div>
           </div>
 
@@ -229,24 +218,24 @@ class Profile extends React.Component {
             <label>教育程度</label>
             <div className='profile-form-component-group'>
               <div className='radiobox form-component-theme-gray'>
-                <input id='junior-high-school' type='radio' name='education-level' />
-                <label htmlFor='junior-high-school'>國中</label>
+                <input id='junior' type='radio' name='education-level' />
+                <label htmlFor='junior' data-element-name='educationLevel'>國中</label>
               </div>
               <div className='radiobox form-component-theme-gray'>
-                <input id='high-school' type='radio' name='education-level' />
-                <label htmlFor='high-school'>高中</label>
+                <input id='senior' type='radio' name='education-level' />
+                <label htmlFor='senior' data-element-name='educationLevel'>高中</label>
               </div>
               <div className='radiobox form-component-theme-gray'>
-                <input id='university' type='radio' name='education-level' />
-                <label htmlFor='university'>大學</label>
+                <input id='bachelor' type='radio' name='education-level' />
+                <label htmlFor='bachelor' data-element-name='educationLevel'>大學</label>
               </div>
               <div className='radiobox form-component-theme-gray'>
-                <input id='master-degree' type='radio' name='education-level' />
-                <label htmlFor='master-degree'>碩士</label>
+                <input id='master' type='radio' name='education-level' />
+                <label htmlFor='master' data-element-name='educationLevel'>碩士</label>
               </div>
               <div className='radiobox form-component-theme-gray'>
-                <input id='doctorate-degree' type='radio' name='education-level' />
-                <label htmlFor='doctorate-degree'>博士</label>
+                <input id='doctor' type='radio' name='education-level' />
+                <label htmlFor='doctor' data-element-name='educationLevel'>博士</label>
               </div>
             </div>
             <div className='profile-form-component-description'>此數據僅作分析用途，不會透露給其他用戶。</div>
