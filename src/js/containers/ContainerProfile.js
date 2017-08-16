739e7528a2e74f6dbcf246b7a8e03e48e70328dd
Justin Ho <mujan5427@gmail.com>
2017-08-16 14:39:42 +0800

    [update] mapStateToProps function 1. redefine structure of props 2. unification data type of props

--- src/js/containers/ContainerProfile.js
+++ src/js/containers/ContainerProfile.js
@@ -3,11 +3,40 @@ import Profile from '../components/layout/Profile';
 
 
 function getSpecifiedProps(pageProfileOfState, specifiedProps) {
-  if (!isEmpty(pageProfileOfState) && !isEmpty(pageProfileOfState.content[specifiedProps])) {
-    return pageProfileOfState.content[specifiedProps];
+  if (!isEmpty(pageProfileOfState) && !isEmpty(pageProfileOfState.content)) {
+    return !isEmpty(pageProfileOfState.content[specifiedProps]) ?
+    pageProfileOfState.content[specifiedProps] :
+    '';
   }
 
   return '';
+}
+
+function getSpecifiedPartOfBirthday(birthday, specifiedProperty) {
+  const year  = birthday.split('-')[0];
+  const month = birthday.split('-')[1];
+  const day   = birthday.split('-')[2];
+
+  switch(specifiedProperty) {
+    case 'year':
+      return year;
+
+    case 'month':
+      return month;
+
+    case 'day':
+      return day;
+  }
+}
+
+function getSpecifiedPartOfInterest(interest, specifiedProperty) {
+  const indexOfSearchResult = interest.indexOf(specifiedProperty);
+
+  if(indexOfSearchResult !== -1) {
+    return true;
+  }
+
+  return false;
 }
 
 function mapStateToProps(state) {
@@ -18,11 +47,33 @@ function mapStateToProps(state) {
     firstname: getSpecifiedProps(pageProfile, 'first_name'),
     lastname: getSpecifiedProps(pageProfile, 'last_name'),
     gender: getSpecifiedProps(pageProfile, 'gender'),
-    birthday: getSpecifiedProps(pageProfile, 'birthday'),
+    year: !isEmpty(getSpecifiedProps(pageProfile, 'birthday')) ?
+    getSpecifiedPartOfBirthday(getSpecifiedProps(pageProfile, 'birthday'), 'year') :
+    '',
+    month: !isEmpty(getSpecifiedProps(pageProfile, 'birthday')) ?
+    getSpecifiedPartOfBirthday(getSpecifiedProps(pageProfile, 'birthday'), 'month') :
+    '',
+    day: !isEmpty(getSpecifiedProps(pageProfile, 'birthday')) ?
+    getSpecifiedPartOfBirthday(getSpecifiedProps(pageProfile, 'birthday'), 'day') :
+    '',
     email: getSpecifiedProps(pageProfile, 'email'),
     language: getSpecifiedProps(pageProfile, 'language'),
     educationLevel: getSpecifiedProps(pageProfile, 'education_level'),
-    interest: getSpecifiedProps(pageProfile, 'interest')
+    sport: !isEmpty(getSpecifiedProps(pageProfile, 'interest')) ?
+    getSpecifiedPartOfInterest(getSpecifiedProps(pageProfile, 'interest'), 'sport') :
+    false,
+    handmade: !isEmpty(getSpecifiedProps(pageProfile, 'interest')) ?
+    getSpecifiedPartOfInterest(getSpecifiedProps(pageProfile, 'interest'), 'hand_made') :
+    false,
+    baking: !isEmpty(getSpecifiedProps(pageProfile, 'interest')) ?
+    getSpecifiedPartOfInterest(getSpecifiedProps(pageProfile, 'interest'), 'baking') :
+    false,
+    art: !isEmpty(getSpecifiedProps(pageProfile, 'interest')) ?
+    getSpecifiedPartOfInterest(getSpecifiedProps(pageProfile, 'interest'), 'art') :
+    false,
+    history: !isEmpty(getSpecifiedProps(pageProfile, 'interest')) ?
+    getSpecifiedPartOfInterest(getSpecifiedProps(pageProfile, 'interest'), 'history') :
+    false
   };
 }
 
