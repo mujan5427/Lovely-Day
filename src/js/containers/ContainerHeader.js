0e77fdd7207905d1613eb0d15fddaf7f4f39c5e3
Justin Ho <mujan5427@gmail.com>
2017-08-17 23:29:05 +0800

    [update] mapStateToProps function 1. make logic of getting user name is a independent function 2. add selectedNavigationList props 3. add selectedNavigationType props

--- src/js/containers/ContainerHeader.js
+++ src/js/containers/ContainerHeader.js
@@ -3,19 +3,54 @@ import { getCookie } from '../helpers/cookie';
 import Header from '../components/layout/Header';
 
 
-function mapStateToProps(state) {
+function getUsername() {
   var userName = undefined;
   var cookie = getCookie();
 
   if (!isEmpty(cookie.first_name)) {
-    userName = cookie.first_name;
+    return userName = cookie.first_name;
   }
 
+  return userName;
+}
+
+function getSelectedNavigationList(state) {
+  var entities, navigationList, selected;
+
+  if (state.headerNavigation.hasOwnProperty('navigationList') &&
+      !isEmpty(state.headerNavigation.navigationList &&
+      !isEmpty(state.entities.experiences))) {
+        entities       = state.entities.experiences;
+        selected       = state.headerNavigation.selected;
+        navigationList = state.headerNavigation.navigationList[selected];
+
+    return navigationList.map(index => entities[index]);
+
+  } else {
+    return undefined;
+
+  }
+}
+
+function getSelectedNavigationType(state) {
+  if (state.headerNavigation.hasOwnProperty('selected') &&
+      !isEmpty(state.headerNavigation.selected)) {
+
+    return state.headerNavigation.selected;
+  } else {
+    return undefined;
+
+  }
+}
+
+function mapStateToProps(state) {
   return {
     hasLoggedIn: state.hasLoggedIn,
-    userName: userName,
+    userName: getUsername(),
     displayDialogAccount: state.displayDialogAccount,
-    displayMenu: state.displayMenu
+    displayMenu: state.displayMenu,
+    selectedNavigationList: getSelectedNavigationList(state),
+    selectedNavigationType: getSelectedNavigationType(state)
   };
 }
 
