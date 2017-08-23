47fab2e8f7fc66a8ae9c53d1cc8c793aec0a475c
Justin Ho <mujan5427@gmail.com>
2017-08-23 20:21:23 +0800

    [add] `recommendationList` props

--- src/js/containers/ContainerExperienceDetail.js
+++ src/js/containers/ContainerExperienceDetail.js
@@ -13,6 +13,55 @@ function getSpecifiedEntity(state, propertyOfEntity) {
   return undefined;
 }
 
+function getRecommendationListByType(state, currentType) {
+  const navigationList = state.headerNavigation.navigationList;
+  const entity         = state.entities.experiences;
+  var recommendationListIndex, recommendationList;
+
+  if(!isEmpty(currentType) && !isEmpty(navigationList) && !isEmpty(entity)) {
+    switch(currentType) {
+      case 'outdoor':
+        recommendationListIndex = navigationList['summer_camp'];
+        break;
+
+      case 'summer_camp':
+        recommendationListIndex = navigationList['baking'];
+        break;
+
+      case 'baking':
+        recommendationListIndex = navigationList['lover'];
+        break;
+
+      case 'lover':
+        recommendationListIndex = navigationList['group'];
+        break;
+
+      case 'group':
+        recommendationListIndex = navigationList['play_with_child'];
+        break;
+
+      case 'play_with_child':
+        recommendationListIndex = navigationList['hand_made'];
+        break;
+
+      case 'hand_made':
+        recommendationListIndex = navigationList['outdoor'];
+        break;
+
+      default:
+        break;
+    }
+
+    // remove the last element of recommendationListIndex
+    recommendationListIndex = recommendationListIndex.filter((item, index) => index <= 2);
+    recommendationList      = recommendationListIndex.map(item => entity[item]);
+
+    return recommendationList;
+  }
+
+  return undefined;
+}
+
 function mapStateToProps(state) {
   return {
     displayContent: state.pageExperienceDetail.displayContent,
@@ -25,7 +74,8 @@ function mapStateToProps(state) {
     cancelMethod: getSpecifiedEntity(state, 'cancel_method'),
     images: getSpecifiedEntity(state, 'images'),
     host: getSpecifiedEntity(state, 'host'),
-    favorited: getSpecifiedEntity(state, 'favorited')
+    favorited: getSpecifiedEntity(state, 'favorited'),
+    recommendationList: getRecommendationListByType(state, getSpecifiedEntity(state, 'type'))
   };
 }
 
