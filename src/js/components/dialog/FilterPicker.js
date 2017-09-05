310ce0a33b070e95e1b9e3c8725e61941cbdab9e
Justin Ho <mujan5427@gmail.com>
2017-09-06 01:15:12 +0800

    [update] integrate `cancelHandler` and `confirmHandler` props

--- src/js/components/dialog/FilterPicker.js
+++ src/js/components/dialog/FilterPicker.js
@@ -3,10 +3,25 @@ import React from 'react';
 class FilterPicker extends React.Component {
   constructor(props) {
     super(props);
+
+    this.cancelClickHandler  = this.cancelClickHandler.bind(this);
+    this.confirmClickHandler = this.confirmClickHandler.bind(this);
+  }
+
+  cancelClickHandler() {
+    const { type, cancelHandler } = this.props;
+
+    cancelHandler(type);
+  }
+
+  confirmClickHandler() {
+    const { type, confirmHandler } = this.props;
+
+    confirmHandler(type);
   }
 
   render() {
-    const { children, cancelHandler } = this.props;
+    const { children } = this.props;
 
     return (
       <div className='filter-picker-background'>
@@ -16,8 +31,8 @@ class FilterPicker extends React.Component {
           </section>
 
           <section>
-            <a onClick={ cancelHandler }>取消</a>
-            <a>確定</a>
+            <a onClick={ this.cancelClickHandler }>取消</a>
+            <a onClick={ this.confirmClickHandler }>確定</a>
           </section>
         </div>
       </div>
