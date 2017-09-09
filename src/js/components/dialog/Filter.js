e03f791dde4f6e50cb47dd2ede5f5f056f3edbfa
Justin Ho <mujan5427@gmail.com>
2017-09-09 17:11:30 +0800

    [update] Filter component 1. remove CSS declaration of hardcode 2. add `dialog-have-header-footer` CSS declaration 3. update `dialog-box-background` and `dialog-content`

--- src/js/components/dialog/Filter.js
+++ src/js/components/dialog/Filter.js
@@ -6,21 +6,11 @@ import Footer from './common/Footer';
 class Filter extends React.Component {
   constructor(props) {
     super(props);
-
-    this.dialogContentCSS = {
-      'border-bottom': '1px solid',
-      'border-top': '1px solid',
-      'font-size': '1.1875em',
-      height: 'calc(100% - 64px - 72px)'
-    };
-
-    this.dialogCheckboxCSS = {
-      display: 'block',
-      margin: '16px 0'
-    };
   }
 
   render() {
+    const { children } = this.props;
+
     return (
       <Wrapper>
 
@@ -28,45 +18,8 @@ class Filter extends React.Component {
         <Header type='TYPE-2' title='篩選條件' />
 
         {/* Content */}
-        <div className='dialog-content' style={ this.dialogContentCSS }>
-
-          {/* Dialog Section : Region */}
-          <section>
-            <div className='dialog-item-name'>地區</div>
-            <div className='checkbox form-component-theme-gray' style={ this.dialogCheckboxCSS }>
-              <input id='region-1' type='checkbox' />
-              <label htmlFor='region-1'>大台北</label>
-            </div>
-            <div className='checkbox form-component-theme-gray' style={ this.dialogCheckboxCSS }>
-              <input id='region-2' type='checkbox' />
-              <label htmlFor='region-2'>桃竹苗</label>
-            </div>
-            <div className='checkbox form-component-theme-gray' style={ this.dialogCheckboxCSS }>
-              <input id='region-3' type='checkbox' />
-              <label htmlFor='region-3'>宜蘭</label>
-            </div>
-            <div className='checkbox form-component-theme-gray' style={ this.dialogCheckboxCSS }>
-              <input id='region-4' type='checkbox' />
-              <label htmlFor='region-4'>中彰投</label>
-            </div>
-          </section>
-
-          {/* Dialog Section : Type */}
-          <section>
-            <div className='dialog-item-name'>類型</div>
-            <div className='checkbox form-component-theme-gray' style={ this.dialogCheckboxCSS }>
-              <input id='type-1' type='checkbox' />
-              <label htmlFor='type-1'>藝文手作</label>
-            </div>
-            <div className='checkbox form-component-theme-gray' style={ this.dialogCheckboxCSS }>
-              <input id='type-2' type='checkbox' />
-              <label htmlFor='type-2'>玩樂廚房</label>
-            </div>
-            <div className='checkbox form-component-theme-gray' style={ this.dialogCheckboxCSS }>
-              <input id='type-3' type='checkbox' />
-              <label htmlFor='type-3'>愛上戶外</label>
-            </div>
-          </section>
+        <div className='dialog-content dialog-have-header-footer'>
+          { children }
         </div>
 
         {/* Footer */}
