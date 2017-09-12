600c3b302d4e5aa34bd7e63b2914a284182eb199
Justin Ho <mujan5427@gmail.com>
2017-09-12 17:11:01 +0800

    [update] ContainerSearch component 1. add `getExperienceList` function 2. add `needUpdate`、`isThisLastPage` and `experienceList` props

--- src/js/containers/ContainerSearch.js
+++ src/js/containers/ContainerSearch.js
@@ -1,13 +1,35 @@
 import { connect } from 'react-redux';
+import { hasPropertyInDeep } from '../helpers/object';
 import Search from '../components/layout/Search';
 
+
+function getExperienceList(state) {
+  if(!isEmpty(state.entities.experiences) && hasPropertyInDeep(state.pageSearch, 'experienceList.items')) {
+    const experience          = state.entities.experiences;
+    const experienceListIndex = state.pageSearch.experienceList.items;
+
+    return experienceListIndex.map(item => ({
+      id: experience[item].id,
+      title: experience[item].title,
+      image: experience[item].images[0],
+      price: experience[item].price,
+      favorited: experience[item].favorited
+    }));
+
+  }
+
+  return undefined;
+}
 
 function mapStateToProps(state) {
   return {
     hasLoggedIn: state.hasLoggedIn,
     displayFilterPickerRegion: state.displaySearch.displayFilterPickerRegion,
     displayFilterPickerType:state.displaySearch.displayFilterPickerType,
-    displayDialogFilter: state.displaySearch.displayDialogFilter
+    displayDialogFilter: state.displaySearch.displayDialogFilter,
+    needUpdate: !isEmpty(state.pageSearch.experienceList) ? state.pageSearch.experienceList.needUpdate : false,
+    isThisLastPage: !isEmpty(state.pageSearch.experienceList) ? state.pageSearch.experienceList.isThisLastPage : undefined,
+    experienceList: getExperienceList(state)
   };
 }
 
