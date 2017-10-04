c8038562a07a513a717f1eedf1e8cfcfe3ae06fd
Justin Ho <mujan5427@gmail.com>
2017-10-04 18:06:42 +0800

    [update] Menu Navigation component 1. update target patch 2. update sorting of menu item

--- src/js/components/menu/Navigation.js
+++ src/js/components/menu/Navigation.js
@@ -37,37 +37,33 @@ class Navigation extends React.Component {
           </header>
 
           <section className='menu-list menu-item-theme-navigation' onClick={ this.hiddenMenu }>
-            <Link to='/experiences/1' data-can-be-triggered-element>
-              <i className='fa fa-child fa-fw' aria-hidden='true'></i>
-              <label>親子專區</label>
-            </Link>
-            <Link to='/experiences/1' data-can-be-triggered-element>
-              <i className='fa fa-heart fa-fw' aria-hidden='true'></i>
-              <label>情人專區</label>
-            </Link>
-            <Link to='/experiences/1' data-can-be-triggered-element>
-              <i className='fa fa-cutlery fa-fw' aria-hidden='true'></i>
-              <label>玩樂廚房</label>
-            </Link>
-            <Link to='/experiences/1' data-can-be-triggered-element>
-              <i className='fa fa-american-sign-language-interpreting fa-fw' aria-hidden='true'></i>
-              <label>藝文手作</label>
-            </Link>
-            <Link to='/experiences/1' data-can-be-triggered-element>
-              <i className='fa fa-tree fa-fw' aria-hidden='true'></i>
-              <label>愛上戶外</label>
-            </Link>
-            <Link to='/experiences/1' data-can-be-triggered-element>
-              <i className='fa fa-users fa-fw' aria-hidden='true'></i>
-              <label>團體遊戲</label>
-            </Link>
-            <Link to='/experiences/1' data-can-be-triggered-element>
+            <Link to='/search?category=1' data-can-be-triggered-element>
               <i className='fa fa-flag-checkered  fa-fw' aria-hidden='true'></i>
               <label>夏令營專區</label>
             </Link>
-            <Link to='/experiences/1' data-can-be-triggered-element>
-              <i className='fa fa-birthday-cake fa-fw' aria-hidden='true'></i>
-              <label>活動包場</label>
+            <Link to='/search?category=2' data-can-be-triggered-element>
+              <i className='fa fa-american-sign-language-interpreting fa-fw' aria-hidden='true'></i>
+              <label>藝文手作</label>
+            </Link>
+            <Link to='/search?category=3' data-can-be-triggered-element>
+              <i className='fa fa-cutlery fa-fw' aria-hidden='true'></i>
+              <label>玩樂廚房</label>
+            </Link>
+            <Link to='/search?category=4' data-can-be-triggered-element>
+              <i className='fa fa-tree fa-fw' aria-hidden='true'></i>
+              <label>愛上戶外</label>
+            </Link>
+            <Link to='/search?category=5' data-can-be-triggered-element>
+              <i className='fa fa-child fa-fw' aria-hidden='true'></i>
+              <label>親子專區</label>
+            </Link>
+            <Link to='/search?category=6' data-can-be-triggered-element>
+              <i className='fa fa-users fa-fw' aria-hidden='true'></i>
+              <label>團體遊戲</label>
+            </Link>
+            <Link to='/search?category=7' data-can-be-triggered-element>
+              <i className='fa fa-heart fa-fw' aria-hidden='true'></i>
+              <label>情人專區</label>
             </Link>
           </section>
         </div>
