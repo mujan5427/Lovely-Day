66f44708427b5212e461e6db7403c43bd394dcc2
Justin Ho <mujan5427@gmail.com>
2017-10-19 00:59:55 +0800

    [update] Header component 1. remove `hiddenMenu` and `removeStyleAttribute` methods 2. add `showDropDownMenuNavigation` method 3. add `hiddenDropDownMenuNavigation` method 4. add `showDropDownMenuPersonalInformation` method 5. add `hiddenDropDownMenuPersonalInformation` method 6. integrate these new methods with `navigation` and `personal information` of drop down menu element 7. integrate `hiddenDropDownMenuPersonalInformation` method with `logoutButton` method

--- src/js/components/layout/Header.js
+++ src/js/components/layout/Header.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { getCookie, verifyCookie } from '../../helpers/cookie';
-import { toggleHasLoggedIn, toggleDisplayDialogSignup, toggleDisplayDialogLogin, toggleDisplayMenuMain, toggleDisplayMenuNavigation, logout, fetchData, modifyNavigationType, GROUP_HEADER_NAVIGATION } from '../../actions/action';
+import { toggleHasLoggedIn, toggleDisplayDialogSignup, toggleDisplayDialogLogin, toggleDisplayMenuMain, toggleDisplayMenuNavigation, logout, fetchData, modifyNavigationType,
+enableDisplayDropDownMenuNavigation, disableDisplayDropDownMenuNavigation,
+enableDisplayDropDownMenuPersonalInformation, disableDisplayDropDownMenuPersonalInformation,
+GROUP_HEADER_NAVIGATION } from '../../actions/action';
 
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
 
-    this.hiddenMenu           = this.hiddenMenu.bind(this);
-    this.removeStyleAttribute = this.removeStyleAttribute.bind(this);
-    this.logoutButton         = this.logoutButton.bind(this);
-    this.toggleDialogLogin    = this.toggleDialogLogin.bind(this)
-    this.toggleDialogSignup   = this.toggleDialogSignup.bind(this);
-    this.toggleMenuMain       = this.toggleMenuMain.bind(this);
-    this.toggleMenuNavigation = this.toggleMenuNavigation.bind(this);
-    this.getNavigationTitle   = this.getNavigationTitle.bind(this);
-    this.mouseEnterHandler    = this.mouseEnterHandler.bind(this);
+    this.logoutButton                          = this.logoutButton.bind(this);
+    this.toggleDialogLogin                     = this.toggleDialogLogin.bind(this)
+    this.toggleDialogSignup                    = this.toggleDialogSignup.bind(this);
+    this.toggleMenuMain                        = this.toggleMenuMain.bind(this);
+    this.toggleMenuNavigation                  = this.toggleMenuNavigation.bind(this);
+    this.getNavigationTitle                    = this.getNavigationTitle.bind(this);
+    this.mouseEnterHandler                     = this.mouseEnterHandler.bind(this);
+    this.showDropDownMenuNavigation            = this.showDropDownMenuNavigation.bind(this);
+    this.hiddenDropDownMenuNavigation          = this.hiddenDropDownMenuNavigation.bind(this);
+    this.showDropDownMenuPersonalInformation   = this.showDropDownMenuPersonalInformation.bind(this);
+    this.hiddenDropDownMenuPersonalInformation = this.hiddenDropDownMenuPersonalInformation.bind(this);
   }
 
   componentWillMount() {
@@ -29,21 +34,10 @@ class Header extends React.Component {
     }
   }
 
-  hiddenMenu(event) {
-    if (!event.target.hasAttribute('data-can-be-triggered-element')) {
-      event.preventDefault();
-    } else {
-      event.currentTarget.style.display = 'none';
-    }
-  }
-
-  removeStyleAttribute(event) {
-    event.currentTarget.style.display = null;
-  }
-
   logoutButton() {
     const { dispatch } = this.props;
 
+    this.hiddenDropDownMenuPersonalInformation();
     dispatch(logout());
   }
 
@@ -110,8 +104,33 @@ class Header extends React.Component {
     }
   }
 
+  showDropDownMenuNavigation() {
+    const { dispatch } = this.props;
+
+    dispatch(enableDisplayDropDownMenuNavigation());
+  }
+
+  hiddenDropDownMenuNavigation() {
+    const { dispatch } = this.props;
+
+    dispatch(disableDisplayDropDownMenuNavigation());
+  }
+
+  showDropDownMenuPersonalInformation() {
+    const { dispatch } = this.props;
+
+    dispatch(enableDisplayDropDownMenuPersonalInformation());
+  }
+
+  hiddenDropDownMenuPersonalInformation() {
+    const { dispatch } = this.props;
+
+    dispatch(disableDisplayDropDownMenuPersonalInformation());
+  }
+
   render() {
-    const { hasLoggedIn = false, userName = 'user name', selectedNavigationList } = this.props;
+    const { hasLoggedIn = false, userName = 'user name', selectedNavigationList,
+            dropDownMenuNavigation, dropDownMenuPersonalInformation } = this.props;
     const { displayMenuMain } = this.props.displayMenu;
 
     return (
@@ -154,50 +173,51 @@ class Header extends React.Component {
             <Link to='/search'>
               <i className='fa fa-search' aria-hidden='true'></i>
             </Link>
-            <a className='header-menu-navigation hasIcon'>
+            <a
+              className='header-menu-navigation hasIcon'
+              onMouseEnter={ this.showDropDownMenuNavigation }
+              onMouseLeave={ this.hiddenDropDownMenuNavigation }
+            >
               類型
               <i className='fa fa-angle-down' aria-hidden='true'></i>
-            </a>
 
-            {/* Drop-Down-Menu Navigation */}
-            { selectedNavigationList &&
-              <div
-                className='drop-down-menu-wrapper drop-down-menu-type-2'
-                onClick={ this.hiddenMenu }
-                onMouseLeave={ this.removeStyleAttribute }
-              >
-                <div>
-                  <section>
-                    <h1>最新的 { this.getNavigationTitle() }</h1>
-                    <div>
-                      {
-                        selectedNavigationList.map(item => {
-                          return (
-                            <Link
-                              to={ `/experiences/${ item.id }` } data-can-be-triggered-element
-                            >
-                              <img src={ item.images[0] } />
-                              <figcaption>{ item.title }</figcaption>
-                            </Link>
-                          );
-                        })
-                      }
-                    </div>
-                  </section>
-                  <section>
-                    <nav>
-                      <a data-navigation-type='summer_camp' onMouseEnter={ this.mouseEnterHandler }>夏令營專區</a>
-                      <a data-navigation-type='hand_made' onMouseEnter={ this.mouseEnterHandler }>藝文手作</a>
-                      <a data-navigation-type='baking' onMouseEnter={ this.mouseEnterHandler }>玩樂廚房</a>
-                      <a data-navigation-type='outdoor' onMouseEnter={ this.mouseEnterHandler }>愛上戶外</a>
-                      <a data-navigation-type='play_with_child' onMouseEnter={ this.mouseEnterHandler }>親子專區</a>
-                      <a data-navigation-type='group' onMouseEnter={ this.mouseEnterHandler }>團體遊戲</a>
-                      <a data-navigation-type='lover' onMouseEnter={ this.mouseEnterHandler }>情人專區</a>
-                    </nav>
-                  </section>
+              {/* Drop-Down-Menu Navigation */}
+              { dropDownMenuNavigation &&
+                <div className='drop-down-menu-wrapper drop-down-menu-type-2'>
+                  <div>
+                    <section>
+                      <h1>最新的 { this.getNavigationTitle() }</h1>
+                      <div>
+                        {
+                          selectedNavigationList.map(item => {
+                            return (
+                              <Link
+                                to={ `/experiences/${ item.id }` }
+                                onClick={ this.hiddenDropDownMenuNavigation }
+                              >
+                                <img src={ item.images[0] } />
+                                <figcaption>{ item.title }</figcaption>
+                              </Link>
+                            );
+                          })
+                        }
+                      </div>
+                    </section>
+                    <section>
+                      <nav>
+                        <a data-navigation-type='summer_camp' onMouseEnter={ this.mouseEnterHandler }>夏令營專區</a>
+                        <a data-navigation-type='hand_made' onMouseEnter={ this.mouseEnterHandler }>藝文手作</a>
+                        <a data-navigation-type='baking' onMouseEnter={ this.mouseEnterHandler }>玩樂廚房</a>
+                        <a data-navigation-type='outdoor' onMouseEnter={ this.mouseEnterHandler }>愛上戶外</a>
+                        <a data-navigation-type='play_with_child' onMouseEnter={ this.mouseEnterHandler }>親子專區</a>
+                        <a data-navigation-type='group' onMouseEnter={ this.mouseEnterHandler }>團體遊戲</a>
+                        <a data-navigation-type='lover' onMouseEnter={ this.mouseEnterHandler }>情人專區</a>
+                      </nav>
+                    </section>
+                  </div>
                 </div>
-              </div>
-            }
+              }
+            </a>
 
             <a>幫助</a>
 
@@ -211,23 +231,30 @@ class Header extends React.Component {
 
             {/* has logged in */}
             { hasLoggedIn &&
-              <a className='header-menu-main'>{ userName }</a>
-            }
-
-            {/* Drop-Down-Menu Main */}
-            { hasLoggedIn &&
-              <div
-                className='drop-down-menu-wrapper drop-down-menu-type-1'
-                onClick={ this.hiddenMenu }
-                onMouseLeave={ this.removeStyleAttribute }
+              <a
+                className='header-menu-main'
+                onMouseEnter={ this.showDropDownMenuPersonalInformation }
+                onMouseLeave={ this.hiddenDropDownMenuPersonalInformation }
               >
-                <nav>
-                  <Link to='/profile' data-can-be-triggered-element>個人資料</Link>
-                  <a>訊息</a>
-                  <a>心願單</a>
-                  <a onClick={ this.logoutButton }>登出</a>
-                </nav>
-              </div>
+                { userName }
+
+                {/* Drop-Down-Menu Personal Information */}
+                { dropDownMenuPersonalInformation &&
+                  <div className='drop-down-menu-wrapper drop-down-menu-type-1'>
+                    <nav>
+                      <Link
+                        to='/profile'
+                        onClick={ this.hiddenDropDownMenuPersonalInformation }
+                      >
+                        個人資料
+                      </Link>
+                      <a>訊息</a>
+                      <a>心願單</a>
+                      <a onClick={ this.logoutButton }>登出</a>
+                    </nav>
+                  </div>
+                }
+              </a>
             }
           </nav>
         </section>
