d70f664099df2a1a8319c52cc64c56beb1f6201d
Justin Ho <mujan5427@gmail.com>
2017-10-19 18:23:58 +0800

    [update] Main component 1. remove `hiddenMenu` method 2. add `hiddenMenuPersonalInformation` method 3. integrate `logoutButton`、`toggleDialogLogin` and `toggleDialogSignup` method with `hiddenMenuPersonalInformation` method 4. remove `data-can-be-triggered-element` property of each element 5. replace `hiddenMenu` event handler to `hiddenMenuPersonalInformation` event handler

--- src/js/components/menu/Main.js
+++ src/js/components/menu/Main.js
@@ -1,97 +1,100 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { deleteCookie } from '../../helpers/cookie';
-import { toggleHasLoggedIn, toggleDisplayDialogSignup, toggleDisplayDialogLogin, toggleDisplayMenuMain, logout } from '../../actions/action';
+import { toggleDisplayDialogSignup, toggleDisplayDialogLogin, logout,
+disableDisplayDropDownMenuPersonalInformation
+ } from '../../actions/action';
 
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
 
-    this.hiddenMenu         = this.hiddenMenu.bind(this);
-    this.logoutButton       = this.logoutButton.bind(this);
-    this.toggleDialogSignup = this.toggleDialogSignup.bind(this);
-    this.toggleDialogLogin  = this.toggleDialogLogin.bind(this);
+    this.hiddenMenuPersonalInformation = this.hiddenMenuPersonalInformation.bind(this);
+    this.logoutButton                  = this.logoutButton.bind(this);
+    this.toggleDialogSignup            = this.toggleDialogSignup.bind(this);
+    this.toggleDialogLogin             = this.toggleDialogLogin.bind(this);
   }
 
-  hiddenMenu(event) {
+  hiddenMenuPersonalInformation() {
     const { dispatch } = this.props;
 
-    if (event.target.hasAttribute('data-can-be-triggered-element')) {
-      dispatch(toggleDisplayMenuMain());
-    }
+    dispatch(disableDisplayDropDownMenuPersonalInformation());
   }
 
   logoutButton(event) {
     const { dispatch } = this.props;
 
+    this.hiddenMenuPersonalInformation();
     dispatch(logout());
   }
 
   toggleDialogLogin() {
     const { dispatch } = this.props;
 
+    this.hiddenMenuPersonalInformation();
     dispatch(toggleDisplayDialogLogin());
   }
 
   toggleDialogSignup() {
     const { dispatch } = this.props;
 
+    this.hiddenMenuPersonalInformation();
     dispatch(toggleDisplayDialogSignup());
   }
 
   render() {
-    const { hasLoggedIn } = this.props;
-    const { displayMenuMain } = this.props.displayMenu;
+    const { hasLoggedIn, dropDownMenuPersonalInformation } = this.props;
 
-    return (
-      <div
-        className='menu-wrapper'
-        style={ displayMenuMain ? {display: 'block'} : null }
-        onClick={ this.hiddenMenu }
-      >
-        { hasLoggedIn &&
-          <div>
+    if(dropDownMenuPersonalInformation) {
+      return (
+        <div className='menu-wrapper'>
+          { hasLoggedIn &&
+            <div>
 
-            {/* has logged in */}
-            <section className='menu-list menu-item-theme-main'>
-              <Link to='/profile' data-can-be-triggered-element>
-                <label>個人資料</label>
-                <i className='fa fa-user-o fa-fw' aria-hidden='true'></i>
-              </Link>
-              <a>
-                <label>訊息</label>
-                <i className='fa fa-comment-o fa-fw' aria-hidden='true'></i>
-              </a>
-              <a>
-                <label>心願單</label>
-                <i className='fa fa-heart-o fa-fw' aria-hidden='true'></i>
-              </a>
-            </section>
+              {/* has logged in */}
+              <section className='menu-list menu-item-theme-main'>
+                <Link to='/profile' onClick={ this.hiddenMenuPersonalInformation }>
+                  <label>個人資料</label>
+                  <i className='fa fa-user-o fa-fw' aria-hidden='true'></i>
+                </Link>
+                <a>
+                  <label>訊息</label>
+                  <i className='fa fa-comment-o fa-fw' aria-hidden='true'></i>
+                </a>
+                <a>
+                  <label>心願單</label>
+                  <i className='fa fa-heart-o fa-fw' aria-hidden='true'></i>
+                </a>
+              </section>
 
-            <section className='menu-list menu-item-theme-main'>
-              <a>幫助</a>
-              <a onClick={ this.logoutButton } data-can-be-triggered-element>登出</a>
-            </section>
-          </div>
-        }
+              <section className='menu-list menu-item-theme-main'>
+                <a>幫助</a>
+                <a onClick={ this.logoutButton }>登出</a>
+              </section>
+            </div>
+          }
 
-        { !hasLoggedIn &&
-          <div>
+          { !hasLoggedIn &&
+            <div>
 
-            {/* has logged out */}
-            <section className='menu-list menu-item-theme-main'>
-              <a onClick={ this.toggleDialogSignup } data-can-be-triggered-element>註冊</a>
-              <a onClick={ this.toggleDialogLogin } data-can-be-triggered-element>登入</a>
-            </section>
+              {/* has logged out */}
+              <section className='menu-list menu-item-theme-main'>
+                <a onClick={ this.toggleDialogSignup }>註冊</a>
+                <a onClick={ this.toggleDialogLogin }>登入</a>
+              </section>
 
-            <section className='menu-list menu-item-theme-main'>
-              <a>幫助</a>
-            </section>
-          </div>
-        }
-      </div>
-    );
+              <section className='menu-list menu-item-theme-main'>
+                <a>幫助</a>
+              </section>
+            </div>
+          }
+        </div>
+      );
+    } else {
+      return null;
+    }
+
   }
 }
 
